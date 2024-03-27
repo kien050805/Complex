cd4475348e4e055a864d1e4b1311a5a76f851084
hydukovich <148716031+hydukovich@users.noreply.github.com>
2024-03-27 14:29:06 -0400

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,5 +1,7 @@
 #include "Complex.h"
 #include <iostream>
+#include <cmath>
+
 
 using namespace std;
 
@@ -23,6 +25,90 @@ Complex::Complex(double x, double y)
 
 Complex::~Complex()
 {};
+
+//=======================================================================
+// getReal Accessor
+// Accesses real part of the complex number
+// Nate Hydukovich
+//=======================================================================
+double Complex::getReal() const {
+    return a;
+}
+
+//=======================================================================
+// Operator Equal Too (==)
+// Returns distance of complex from the origin
+// Nate Hydukovich
+//=======================================================================
+bool Complex::operator== (const Complex &c) const {
+    return (a == c.a && b == c.b);
+}
+
+//=======================================================================
+// Operator Add (+), with float
+// Nate Hydukovich
+//=======================================================================
+Complex Complex::operator+ (double f) const {
+    Complex retValue;
+    retValue.a = a + f;
+    retValue.b = b;
+    return retValue;
+}
+
+//=======================================================================
+// Operator Multiply (*), with complex
+// Nate Hydukovich
+//=======================================================================
+Complex Complex::operator* (const Complex &c) const {
+    Complex retValue;
+    retValue.a = a * c.a;
+    retValue.b = b * c.b;
+    return retValue;}
+
+//=======================================================================
+// Operator Subtract (-), with complex
+// Nate Hydukovich
+//=======================================================================
+Complex Complex::operator- (const Complex &c) const {
+    Complex retValue;
+    retValue.a = a - c.a;
+    retValue.b = b - c.b;
+    return retValue;
+}
+
+//=======================================================================
+// Absolute Value
+// Returns distance of complex from the origin
+// Nate Hydukovich
+//=======================================================================
+double Complex::abs (void) const {
+    return(sqrt(a * a + b * b));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ostream & operator<< ( ostream & os, const Complex &c )
 {
