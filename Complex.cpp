66bedac8f64207edaad732f9a50743677f0e1527
Andrew <164203018+andrewnguyxn@users.noreply.github.com>
2024-03-27 23:07:47 -0400

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -102,25 +102,154 @@ double Complex::abs (void) const {
     return(sqrt(a * a + b * b));
 }
 
+//=======================================================================
+// operator/
+// Divide complex number by an integer
+// Andrew Nguyen
+//=======================================================================
+Complex	    Complex::operator/	( double f ) const
+{
+	Complex ret;
 
+	ret.a = a/f;
+    ret.b = b/f;
 
+	return ret;
+}
 
+//=======================================================================
+// operator/
+// Divide complex number by another complex number
+// Andrew Nguyen
+//=======================================================================
+Complex	    Complex::operator/	( int i ) const
+{
+	Complex ret;
 
+	ret.a = a/i;
+    ret.b = b/i;
 
+	return ret;
+}
 
+//=======================================================================
+// operator^
+// Raise complex number by given exponent
+// Andrew Nguyen
+//=======================================================================
+Complex	    Complex::operator^	( int p ) const
+{
+	Complex ret = *this;
 
+    if (p > 0) {
+        for (int i =0; i < p; i++)
+	        ret = ret * *this;
+    }
+    else if (p < 0) {
+        for (int i =0; i < -1*p; i++)
+	        ret = ret / (*this);    
+    }
+    else if (p == 0)
+        return Complex(1,0);
 
+    return ret;
+}
 
+//=======================================================================
+// operator~
+// Return the conjugate of complex number.
+// Andrew Nguyen
+//=======================================================================
+Complex	    Complex::operator~	( void ) const
+{
+	Complex ret;
+    ret.a = a;
+    ret.b = -b;
+
+    return ret;
+}
+
+//=======================================================================
+// operator-
+// Return the negation of complex number.
+// Andrew Nguyen
+//=======================================================================
+Complex	    Complex::operator-	( void ) const
+{
+    Complex ret;
+    ret.a = -a;
+    ret.b = -b;
+
+    return ret;
+}
+
+//=======================================================================
+// cout << operator overload
+// Output the complex number.
+// Andrew Nguyen
+//=======================================================================
 ostream & operator<< ( ostream & os, const Complex &c )
 {
-    ostream & << c.a << c.b;
-    return ;
+    if (c.b == 0 && c.a != 0)
+        os << c.a;
+    else if (c.a == 0 && c.b != 0)
+        os << c.b << "i";
+    else if (c.a == 0 && c.b == 0)
+        os << "0";
+    else if (c.b < 0)
+        os << c.a << c.b << "i";
+    else
+        os << c.a << "+" << c.b << "i";
+
+    return os;
 };
+
+//=======================================================================
+// cin >> operator overload
+// Take the complex number as input.
+// Andrew Nguyen
+//=======================================================================
 istream & operator>> ( istream & is, Complex &c )
 {
+    string s, real, imag;
+    is >> s;
 
-};
-
+    size_t iPos = s.find('i');
+    size_t pos = s.find_last_of("+-");
+    
+    if (iPos != string::npos) {
+        if (iPos != 0) {
+            if (pos != string::npos) {
+                real = s.substr(0, pos);
+                imag = s.substr(pos, s.size()-pos-1);
+    
+                if (real.empty()) 
+                    real = '0';
+                
+                if (imag == "+")
+                    imag = '1';
+                else if (imag == "-")
+                    imag = "-1";
+            }
+            else {
+                real = '0';
+                imag = s.substr(0, iPos);
+            }
+        }
+        else {
+            real = '0';
+            imag = '1';
+        }
+    }
+    else {
+        real = s;
+        imag = '0';
+    }
+    c.a = stof(real);
+    c.b = stof(imag);
+     
+    return is;
+}
 
 
 //=======================================================================
